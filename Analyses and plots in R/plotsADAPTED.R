40b79ca8fe924ef8afd6fb2520f5169183e1e047
Abdallah Al-Janabi <abood@Abdallahs-MacBook-Pro.local>
2024-08-28 19:15:21 +0200

    added folder with R analyses, plots and data generation.

---
+++ Analyses and plots in R/plotsADAPTED.R
@@ -0,0 +1,100 @@
+library(ggplot2)
+library(DALEX)
+library(ggpubr)
+library(latex2exp)
+
+
+
+
+library(ggplot2)
+library(latex2exp)
+importance_labels <- c("5th", "4th", "3rd", "2nd", "1st")
+
+# PVI Confidence Intervals
+
+PVI_ci <- read.csv("/Users/abdallah/Desktop/Kings College Project/Code/results/PVI_confidence_intervals.csv")
+PVI_ci = subset(PVI_ci, select = -c(X) )
+
+ggplot(PVI_ci, aes(x, y)) +        # ggplot2 plot with confidence intervals
+  xlab("Feature") + ylab("Permutation Feature Importance") +
+  geom_point() +
+  geom_errorbar(aes(ymin = lower, ymax = upper), width=0.5,
+                size=0.5,)
+
+
+ggsave("exp2_rsf_PVI_dataset0.pdf", device="pdf", width=1000, height=1200, units="px")
+
+
+
+# dataset 0
+make_factors <- function(data){
+  data$importance_ranking <- factor(data$importance_ranking, levels=sort(unique(data$importance_ranking), decreasing=TRUE))
+  data$variable <- factor(data$variable, levels=rev(c("x4", "x3", "x5", "x1", "x2")))
+  data
+}
+
+barplot_variable_ranking_dataset0 <- function(data, title="", ytitle=""){
+  ggplot(data, aes(fill=variable, y=value, x=importance_ranking)) +
+    geom_bar(position="stack", stat="identity", color="white", size=0.2, width=0.8, 
+             orientation="x") +
+    scale_fill_manual("Global ranking\nof variable importance",
+      values=c("#ae2c87", "#ffa58c", "#8bdcbe", "#46BAC2", "#4378bf"),
+                      limits=c("x4", "x3", "x5", "x1", "x2"), 
+                      labels=c(TeX("$x^{(4)}$"), 
+                               TeX("$x^{(3)}$"), 
+                               TeX("$x^{(5)}$"), 
+                               TeX("$x^{(1)}$"), 
+                               TeX("$x^{(2)}$"))) + 
+    theme_minimal() +
+    scale_y_continuous(expand = c(0, 0), 
+                       breaks=c(seq(0, 100, 10), 102), 
+                       minor_breaks = seq(5, 105, 5)) +
+    scale_x_discrete(labels = importance_labels) +
+    coord_flip() +
+    geom_text(aes(label = value), 
+              position = position_stack(vjust = 0.5), size = 3) + 
+    labs(x=ytitle, y="Number of observations", title=title) +
+    theme(plot.title = element_text(hjust = 0.5), axis.title.x = element_blank()) +
+    guides(fill = guide_legend(title.position = "left", title.hjust = 0.5, title.vjust = 0.5))
+}
+
+
+## Refer to my data
+setwd("/Users/abdallah/Desktop/Kings College Project/Code/results")
+
+# SurvLIME
+exp2_survLIME_n2000_binary_False_PLOT <- read.csv("/Users/abdallah/Desktop/Kings College Project/Code/results/exp2_survLIME_n2000_binary_False_PLOT.csv")
+exp2_survLIME_n2000_binary_False_PLOT = subset(exp2_survLIME_n2000_binary_False_PLOT, select = -c(X) )
+exp2_survLIME_n2000_binary_False_PLOT <- make_factors((exp2_survLIME_n2000_binary_False_PLOT))
+p1_SurvLIME <- barplot_variable_ranking_dataset0(exp2_survLIME_n2000_binary_False_PLOT, ytitle="Importance ranking", title="SurvLIME")
+p1_SurvLIME
+
+
+# SurvSHAP
+exp2_survSHAP_PLOT.csv <- read.csv("/Users/abdallah/Desktop/Kings College Project/Code/results/exp2_survSHAP_PLOT.csv")
+exp2_survSHAP_PLOT.csv = subset(exp2_survSHAP_PLOT.csv, select = -c(X) )
+exp2_survSHAP_PLOT.csv <- make_factors((exp2_survSHAP_PLOT.csv))
+p2_SurvSHAP <- barplot_variable_ranking_dataset0(exp2_survSHAP_PLOT.csv, title="SurvSHAP(t)")
+p2_SurvSHAP
+
+
+# SVSL
+exp2_SVSL_nsamples500_ntotal2000_binary_False_PLOT <- read.csv("/Users/abdallah/Desktop/Kings College Project/Code/results/exp2_SVSL_nsamples500_ntotal2000_binary_False_PLOT.csv")
+exp2_SVSL_nsamples500_ntotal2000_binary_False_PLOT = subset(exp2_SVSL_nsamples500_ntotal2000_binary_False_PLOT, select = -c(X) )
+exp2_SVSL_nsamples500_ntotal2000_binary_False_PLOT <- make_factors((exp2_SVSL_nsamples500_ntotal2000_binary_False_PLOT))
+p3_SVSL <- barplot_variable_ranking_dataset0(exp2_SVSL_nsamples500_ntotal2000_binary_False_PLOT, title="Survival SVSVL")
+p3_SVSL
+
+
+
+
+
+
+p <- ggarrange(p1_SurvLIME, p2_SurvSHAP, p3_SVSL, ncol=3, nrow=1, common.legend = TRUE, legend="bottom") +
+  theme(plot.margin = margin(0.1,0.2,0.1,0.1, "cm")) 
+annotate_figure(p, )
+ggsave("Simulation_Result.pdf", device="pdf", width=2600, height=800, units="px")
+
+
+
+
---
+++ Analyses and plots in R/simulation_evaluation.R
@@ -0,0 +1,72 @@
+# http://www.sthda.com/english/wiki/chi-square-test-of-independence-in-r
+# https://alanarnholt.github.io/PDS-Bookdown2/post-hoc-tests-1.html
+
+exp2_simulation_results_for_chi_square <- read.csv("/Users/abdallah/Desktop/Kings College Project/Code/results/exp2_simulation_results_for_chi_square.csv")
+
+
+exp2_simulation_results_for_chi_square = subset(exp2_simulation_results_for_chi_square, select = -c(X) )
+exp2_simulation_results_for_chi_square
+
+
+
+
+table(exp2_simulation_results_for_chi_square$tool, exp2_simulation_results_for_chi_square$classification)
+test <- chisq.test(table(exp2_simulation_results_for_chi_square$tool, exp2_simulation_results_for_chi_square$classification))
+test # DF are (r-1)*(c-1) which is 3*1 in this case
+
+test$statistic # test statistic
+test$p.value # p-value
+
+
+
+df_sim <- table(exp2_simulation_results_for_chi_square$tool, exp2_simulation_results_for_chi_square$classification)
+
+############
+file_path <- "http://www.sthda.com/sthda/RDoc/data/housetasks.txt"
+housetasks <- read.delim(file_path, row.names = 1)
+chisq <- chisq.test(housetasks)
+chisq
+test
+#################
+
+# Post-hoc comparisons
+df_sim[c(2,3),] # LIME vs SHAP
+post_hoc1 <- chisq.test(df_sim[c(2,3),], correct = FALSE)
+post_hoc1$statistic
+post_hoc1$p.value
+
+
+df_sim[c(3,4),] # SHAP vs SVSL
+post_hoc2 <- chisq.test(df_sim[c(3,4),], correct = FALSE)
+post_hoc2$statistic
+post_hoc2$p.value
+
+
+df_sim[c(2,4),] # LIME vs SVSL
+post_hoc3 <- chisq.test(df_sim[c(2,4),], correct = FALSE)
+post_hoc3$statistic
+post_hoc3$p.value
+
+
+df_sim[c(1,2),] # ChoquEx vs LIME
+post_hoc4 <- chisq.test(df_sim[c(1,2),], correct = FALSE)
+post_hoc4$statistic
+post_hoc4$p.value
+
+
+df_sim[c(1,3),] # ChoquEx vs survSHAP
+post_hoc5 <- chisq.test(df_sim[c(1,3),], correct = FALSE)
+post_hoc5$statistic
+post_hoc5$p.value
+
+
+df_sim[c(1,4),] # ChoquEx vs SVSL
+post_hoc6 <- chisq.test(df_sim[c(1,4),], correct = FALSE)
+post_hoc6$statistic
+post_hoc6$p.value
+
+
+# assumption checks: https://www.statology.org/chi-square-test-assumptions/
+
+test$expected # Chi-square test should only be applied when expected frequency of any cell is at least 5.
+  # observations are independent, both IVs are categorical, cells are mutually exclusive
